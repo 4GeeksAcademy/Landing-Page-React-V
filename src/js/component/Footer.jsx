b95107d2802fc7699cb0f7b3682ef190b79c1081
Valeria Salcedo <153454325+valeriaasalcedoo@users.noreply.github.com>
2024-07-19 06:42:10 +0000

    ready example

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-dark text-white text-center py-3">
+    <p>© 2024 Mi Landing Page. Todos los derechos reservados.</p>
+  </footer>
+);
+
+export default Footer;
---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MyJumbotron = () => (
+  <div className="jumbotron">
+    <div className="container">
+      <h1>A Warm Welcome!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Integer posuere erat a ante.
+      </p>
+      <a className="btn" href="#" role="button">Call to action!</a>
+    </div>
+  </div>
+);
+
+export default MyJumbotron;
